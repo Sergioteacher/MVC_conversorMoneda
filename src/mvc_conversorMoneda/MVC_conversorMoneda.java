0c3f637e239ce8063f85cef7147e9d3c93383fe7
Sergio Teacher <Sergio Teacher@MiniWin>
2023-11-21 13:46:58 +0100

    salida a consola

--- src/mvc_conversorMoneda/MVC_conversorMoneda.java
+++ src/mvc_conversorMoneda/MVC_conversorMoneda.java
@@ -4,7 +4,9 @@ public class MVC_conversorMoneda {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hola salida de consola");
+		System.out.println("...");
+		System.out.println("... para supervisar ...");
 	}
 
 }
